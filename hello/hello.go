8e3d40de2cd3a145efefe7beefa9f6be5f8d9e5f
Clement Ke <skrhbk.ms96@g2.nctu.edu.tw>
2017-08-24 23:15:23 +0800

    Hello Go!

---
+++ hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/skrhbk/golang-learn/stringutil"
+)
+
+func main() {
+	fmt.Printf("hello, world\n")
+	fmt.Printf(stringutil.Reverse("hello, world\n"))
+
+}
